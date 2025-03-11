52b27e520556785a44ed57150823449fe34591e6
ysfkrn63 <ysfkrniawan2@gmail.com>
2025-03-12 03:04:49 +0900

    Ujian week 3

---
+++ quiz/ujian/ujian_week_3.js
@@ -0,0 +1,287 @@
+/*
+Implementasikan function deepSum untuk mendapatkan jumlah pertambahan dari angka-angka yang terdapat di dalam array
+*/
+
+function deepSum (arr) {
+  // Code disini
+  var total = 0;
+  for(var i=0; i < arr.length; i++) {
+   for(var j=0; j < arr[i].length; j++) {
+    for(var k=0; k < arr[i][j].length; k++) {
+     total += parseInt(arr[i][j][k]);
+    }
+   }
+  }  
+  return total === 0 ? "No Number":total;
+}
+
+//TEST CASE
+console.log(deepSum([
+  [
+    [4, 5, 6],
+    [9, 1, 2, 10],
+    [9, 4, 3]
+  ],
+  [
+    [4, 14, 31],
+    [9, 10, 18, 12, 20],
+    [1, 4, 90]
+  ],
+  [
+    [2, 5, 10],
+    [3, 4, 5],
+    [2, 4, 5, 10]
+  ]
+])); // 316
+
+console.log(deepSum([
+  [
+    [20, 10],
+    [15],
+    [1, 1]
+  ],
+  [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    [2],
+    [9, 11]
+  ],
+  [
+    [3, 5, 1],
+    [1, 5, 3],
+    [1]
+  ],
+  [
+    [2]
+  ]
+])); // 156
+
+console.log(deepSum([])); // No number
+
+/*
+Diberikan function naikAngkot(listPenumpang) yang akan menerima satu parameter berupa array dua dimensi. Function akan me-return array of object.
+
+Diberikan sebuah rute, dari A - F. Penumpang diwajibkan membayar Rp2000 setiap melewati satu rute.
+
+Contoh: input: [['Dimitri', 'B', 'F']] output: [{ penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 }]
+*/
+
+function naikAngkot(arrPenumpang) {
+  rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+  //your code here
+  var hasil =[]; 
+  function Person(nama, naik, tujuan) {
+    this.penumpang = nama;
+    this.naikDari = naik;
+    this.tujuan = tujuan;
+    this.bayar = 2000 * (rute.indexOf(tujuan)-rute.indexOf(naik));
+  }
+  for (var i=0; i < arrPenumpang.length; i++) {
+     hasil.push(new Person(arrPenumpang[i][0], arrPenumpang[i][1], arrPenumpang[i][2]));
+  }
+  return JSON.stringify(hasil);
+}
+
+//TEST CASE
+console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
+// [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+//   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+
+console.log(naikAngkot([])); //[]
+
+function highestScore (students) {
+  // Code disini
+  const hasil = {};
+  
+  students.forEach((student) => {
+  if (!hasil[student.class] || student.score > hasil[student.class].score) {
+  hasil[student.class] = student;
+  delete student.class;
+  }
+  });
+  
+  return JSON.stringify(hasil);
+  
+}
+
+// TEST CASE
+console.log(highestScore([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: { name: 'Dimitri', score: 90 },
+//   wolves: { name: 'Alexei', score: 85 }
+// }
+
+
+console.log(highestScore([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: { name: 'Alexander', score: 100 },
+//   wolves: { name: 'Alisa', score: 76 },
+//   tigers: { name: 'Viktor', score: 80 }
+// }
+
+
+console.log(highestScore([])); //{}
+
+/*
+Implementasikan function graduates untuk mendapatkan daftar student yang lulus dengan aturan:
+
+Student dapat dinyatakan lulus apabila score lebih besar dari 75.
+Masukkan name dan score dari student ke class yang dia ikuti.
+Student yang tidak lulus tidak perlu ditampilkan.
+Output yang diharapkan berupa Object dengan format sebagai berikut:
+
+{
+  <class>: [
+    { name: <name>, score: <score> },
+    ...
+  ],
+  <class>: [
+    { name: <name>, score: <score> },
+    ...
+  ],
+  <class>: [] //NOTE: Jika tidak ada student yang lulus, class ini akan diisi oleh array kosong
+}
+*/
+
+function graduates (students) {
+  // Code disini
+ const hasil = {};
+ 
+ students.forEach((student) => {
+ if (student.score >= 75) {
+ if (!hasil[student.class]) {
+ hasil[student.class] = [];
+ }
+ hasil[student.class].push({ name: student.name, score: student.score });
+ }
+ });
+ 
+  
+   return JSON.stringify(hasil);
+}
+
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
